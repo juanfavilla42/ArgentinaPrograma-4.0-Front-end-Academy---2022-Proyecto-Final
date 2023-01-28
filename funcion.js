2f0d6fde8082c5811d86d75845154bb081df4d27
juan jose favilla <juanfavilla23@gmail.com>
2023-01-28 07:31:31 -0300

    Primer escritura en repositorio

---
+++ funcion.js
@@ -0,0 +1,6 @@
+document.getElementById('btn1').addEventListener('click',function(){
+    document.getElementById('sobre_mi').style.display = 'none';
+});
+document.getElementById('btn2').addEventListener('click',function(){
+    document.getElementById('sobre_mi').style.display = 'inline';
+});
